b8225d5713c07b80a8899e20322694dcdc9ad465
Coxhuang <mhesat@outlook.com>
2020-01-21 23:24:20 +0800

    update

--- app/models.py
+++ app/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+class UserProfile(models.Model):
+    """用户信息"""
+    name = models.CharField(verbose_name="姓名",max_length=128)
+    age = models.CharField(verbose_name="年龄",max_length=128)
+    address = models.CharField(verbose_name="地址",max_length=128)
+
+class Students(models.Model):
+    """学生表"""
+    user = models.OneToOneField(to=UserProfile,on_delete=models.DO_NOTHING)
+    stu_id = models.CharField(verbose_name="学号",max_length=128)
+
+class Teachers(models.Model):
+    """老师表"""
+    user = models.OneToOneField(to=UserProfile,on_delete=models.DO_NOTHING)
+    stu = models.ManyToManyField(to=Students)
+
+class Course(models.Model):
+    """课程表"""
+    course_name = models.CharField(verbose_name="课程名",max_length=128)
+    stu = models.ManyToManyField(to=Students)
+    tea = models.ForeignKey(to=Teachers,on_delete=models.DO_NOTHING)
 
 
 
-class MyModels(models.Model):
-
-    # id = models.IntegerField(verbose_name="主键id",primary_key=True)
-    name = models.CharField(verbose_name="姓名",max_length=64)
-    phone = models.CharField(verbose_name="手机号码",max_length=64)
-    email = models.CharField(verbose_name="邮箱地址",max_length=64)
-    add = models.CharField(verbose_name="地址",max_length=128)
-    cast = models.CharField(verbose_name="插入数据需要的时间",max_length=128)
-
-
-class MyTime(models.Model):
-
-    node = models.CharField(verbose_name="节点",max_length=128)
-    time = models.CharField(verbose_name="时刻",max_length=128)
