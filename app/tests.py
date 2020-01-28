1869541a2bcf6cae5eab35894085b9e2ea9729fd
Coxhuang <mhesat@outlook.com>
2020-01-28 23:21:24 +0800

    update

--- app/tests.py
+++ app/tests.py
@@ -9,7 +9,18 @@ from django.test import TestCase
 # a = random.randint(1,10)
 # print(a)
 
+#
+# for foo in range(1,30001):
+#     for j in range(1,4):
+#         print(foo,3*(foo-1)+j)
 
-for foo in range(1,30001):
-    for j in range(1,4):
-        print(foo,3*(foo-1)+j)
+
+import copy
+
+a = ["1","2",["1","3"]]
+print(id(a))
+b = copy.deepcopy(a)
+print(id(b))
+b[2][0] = "88"
+print(a,"---",b)
+
